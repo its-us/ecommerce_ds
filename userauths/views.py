becee0fb0837b19c7848810bbc15963f4e9be79e
ilyas sacha <ilyassacha2@gmail.com>
2024-01-21 01:29:59 +0100

    kan3ml registration

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from userauths.forms import UserRgisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
-# Create your views here.
+def register_view(request):
+
+    if request.method == "POST":
+        form = UserRgisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, Your account was created successfully")
+            new_user = authenticate(username = form.cleaned_data['email'],
+                                    password = form.cleaned_data['password1']
+            )
+            login(request, new_user)
+            return redirect("templates/ecommerec:index")
+   
+    else:
+        print('not registred')
+        form = UserRgisterForm()
+    
+    
+    
+
+    context = {
+        'form': form,
+    }
+    return render(request, "userauths/sign-up.html", context)
