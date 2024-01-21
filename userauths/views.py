bf465d23c6a45918468c1f7c5c8df13a6126b427
Sihame Alami <sihamealami21@gmail.com>
2024-01-21 04:12:56 +0100

    gagdzefdze
    etrfgjh

--- userauths/views.py
+++ userauths/views.py
@@ -1,49 +1,24 @@
 from django.shortcuts import redirect, render
 from userauths.forms import UserRgisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate  # Ajout de la fonction authenticate
 from django.contrib import messages
-from django.conf import settings
-
-User = settings.AUTH_USER_MODEL
 
 def register_view(request):
-
     if request.method == "POST":
         form = UserRgisterForm(request.POST or None)
         if form.is_valid():
             new_user = form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Hey {username}, Your account was created successfully")
-            new_user = authenticate(username = form.cleaned_data['email'],
-                                    password = form.cleaned_data['password1']
-            )
-            login(request, new_user)
-            return redirect("ecommerce:index")
-   
+
+            # Utilisation de la fonction authenticate pour obtenir l'utilisateur
+            user = authenticate(username=form.cleaned_data['email'], password=form.cleaned_data['password1'])
+
+            if user is not None:
+                login(request, user)
+                return redirect("ecommerce:index")
     else:
         form = UserRgisterForm()
-    
-    
-    
 
-    context = {
-        'form': form,
-    }
+    context = {'form': form}
     return render(request, "userauths/sign-up.html", context)
-
-'''
-def login_view(request):
-    if request.user.is_authenticated:
-        return redirect('ecommerce:index')  # Redirect to home page if already logged in
-    
-    if request.method == "POST":
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        
-        try:
-            user = User.objects.get(email = email)
-        except:
-            messages.warning(request, f"User with {email} does not exist")
-            
-        user = authenticate(request, email = email)
-    '''  
