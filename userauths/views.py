12b90e05cb8990e4488b26b6226b5e6c4a92be83
ilyas sacha <ilyassacha2@gmail.com>
2024-01-21 23:29:16 +0100

    login tzadat

--- userauths/views.py
+++ userauths/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import redirect, render
 from userauths.forms import UserRgisterForm
 from django.contrib.auth import login, authenticate  # Ajout de la fonction authenticate
 from django.contrib import messages
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
+
 
 def register_view(request):
     if request.method == "POST":
@@ -26,3 +30,36 @@ def register_view(request):
 
     context = {'form': form}
     return render(request, "userauths/sign-up.html", context)
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("ecommerce:index")
+    
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        try:
+            user  = User.object.get(email = email)
+    
+        except:
+            messages.warning(request, f"User with {email} does not exist.")
+        
+        
+        user = authenticate(request,  email = email, password = password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are logged in.")
+            return redirect("ecommerce:index")
+        
+        else:
+            messages.warning(request, "User does not exist. Create an account.")
+
+
+    context = {
+
+    }
+    
+    return render(request, "userauths/sign-in.html", context)
