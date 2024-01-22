f61202ea172e585719ba4f32b96352a8826231fd
ilyas sacha <ilyassacha2@gmail.com>
2024-01-22 14:51:45 +0100

    new classes added

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -4,40 +4,193 @@ from django.utils.html import mark_safe
 from userauths.models import User
 
 
+STATUS_CHOICE = (
+    ("process", "Processing"),
+    ("shipped", "Shipped"),
+    ("delivered", "Delivered"),
+)
+
+STATUS = (
+    ("draft", "Draft"),
+    ("disabled", "Disabled"),
+    ("rejected", "Rejected"),
+    ("in_review", "In Review"),
+    ("published", "Published"),
+)
+
+RATING = (
+    (1, "⭐✰✰✰✰"),
+    (2, "⭐⭐✰✰✰"),
+    (3, "⭐⭐⭐✰✰"),
+    (4, "⭐⭐⭐⭐✰"),
+    (5, "⭐⭐⭐⭐⭐"),
+)
+
 
 def user_directory_path(instance, filename):
     return 'user_{0}/{1}'.format(instance.user.id, filename)
 
 
 class Category(models.Model):
-    cid = models.UUIDField(unique = True, length = 10, max_Length = 20, Prefix = "cat", alphabet = "abcdefgh12345")
-    title = models.CharField(maxL_Length = 100)
-    image = models.ImageField(upload_to = "category")
+    cid = ShortUUIDField(unique=True, default="cat")
+    title = models.CharField(max_length=100, default="Cloths")
+    image = models.ImageField(upload_to="category", default="category.jpg")
 
     class Meta:
         verbose_name_plural = "Categories"
 
-
     def category_image(self):
-        return mark_safe('<img src= "%s" width="50" height="50 />' % (self.image.url))
-    
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
     def __str__(self):
         return self.title
-    
+
+
+class Tags(models.Model):
+    pass
+
+
 class Vendor(models.Model):
-    vid = models.UUIDField(unique = True, length = 10, max_Length = 20, Prefix = "ven", alphabet = "abcdefgh12345")
-    title = models.CharField(maxL_Length = 100)
-    image = models.ImageField(upload_to = user_directory_path)
-    description = models.TextField(null = True, blank = True)
+    vid = ShortUUIDField(unique=True, default="ven")
+    title = models.CharField(max_length=100, default="Vendor")
+    image = models.ImageField(upload_to=user_directory_path, default="vendor.jpg")
+    description = models.TextField(null=True, blank=True, default="I am an Amazing Vendor")
 
-    adress = models.CharField(maxL_Length = 100, default = "123 casabarata")
-    contact = models.CharField(maxL_Length = 100, default = "+212 65447xx120")
-    chat_resp_time = models.CharField(maxL_Length = 100, default = "100")
-    shipping_on_time = models.CharField(maxL_Length = 100)
-    athentic_rating = models.CharField(maxL_Length = 100)
-    days_return = models.CharField(maxL_Length = 100)
-    warranty_period = models.CharField(maxL_Length = 100)
+    address = models.CharField(max_length=100, default="123 casabarata")
+    contact = models.CharField(max_length=100, default="+212 65447xx120")
+    chat_resp_time = models.CharField(max_length=100, default="100")
+    shipping_on_time = models.CharField(max_length=100)
+    authentic_rating = models.CharField(max_length=100)
+    days_return = models.CharField(max_length=100)
+    warranty_period = models.CharField(max_length=100)
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name_plural = "Vendors"
+
+    def vendor_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
 
 
-    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True)
+class Product(models.Model):
+    pid = ShortUUIDField(unique=True, default="ven")
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=100, default="Product")
+    image = models.ImageField(upload_to=user_directory_path, default="product.jpg")
+    description = models.TextField(null=True, blank=True, default="This is the product")
+    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="0.99")
+    old_price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="2.99")
+    specifications = models.TextField(null=True, blank=True)
+    tags = models.ForeignKey(Tags, on_delete=models.SET_NULL, null=True)
+    product_status = models.CharField(choices=STATUS_CHOICE, max_length=10, default="in_review")
+    status = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    digital = models.BooleanField(default=False)
+    sku = ShortUUIDField(unique=True, default="sku")
 
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />' % (self.image.url))
+
+    def __str__(self):
+        return self.title
+
+    def get_percentage(self):
+        new_price = (self.price / self.old_price) * 100
+        return new_price
+
+
+class ProductImages(models.Model):
+    images = models.ImageField(upload_to="product-images", default="product.jpg")
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Products Images"
+
+
+class CartOrder(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="0.99")
+    paid_status = models.BooleanField(default=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    product_status = models.CharField(choices=STATUS_CHOICE, max_length=30, default="processing")
+
+    class Meta:
+        verbose_name_plural = "Cart Order"
+
+
+class CartOrderItems(models.Model):
+    order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    invoice_no = models.CharField(max_length = 200)
+    product_status = models.CharField(max_length=200)
+    item = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    qty = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="0.99")
+    total = models.DecimalField(max_digits=99999999999999, decimal_places=2, default="0.99")
+
+    class Meta:
+        verbose_name_plural = "Cart Order Items"
+
+    def order_img(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50"/>' % (self.image))
+
+
+class ProductReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    review = models.TextField()
+    rating = models.IntegerField(choices=RATING, default=None)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Product Reviews"
+
+    def __str__(self):
+        return self.product.title
+
+    def get_rating(self):
+        return self.rating
+
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Wishlists"
+
+    def __str__(self):
+        return self.product.title
+
+
+class Address(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=100, null=True)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Addresses"
+
+
+
+class Address(models.Model):
+     user = models.ForeignKey(User, on_delete=models.SET_NULL, null = True)
+     address = models.CharField(max_length = 100, null = True)
+     status = models.BooleanField(default = False)
+
+     class Meta:
+         verbose_name_plural = "Product Reviews"
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
+
+from ecommerce.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReview, Wishlist, Address
+
+
 
 def index(request):
-    return render(request, 'ecommerce/index.html')
+    products = Product.objects.all()
+    context = {
+        "products": products
+
+    }
+
+
+    return render(request, 'ecommerce/index.html', context)
     
