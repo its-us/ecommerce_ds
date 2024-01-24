ec5ba04214d375c90878262b93a0fae44c35f88c
Sihame Alami <sihamealami21@gmail.com>
2024-01-24 17:20:23 +0100

    vendor
    Vendors

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,6 @@
-from ecommerce.views import index
+from ecommerce.views import index, category_list_view, product_list_view, vendor_list_view , vendor_detail_view
 from django.urls import path
-from django.urls import include
+from django.urls import include 
 
 
 app_name = "ecommerce"
@@ -9,5 +9,11 @@ urlpatterns = [
     #Homepage
     path("" , index, name = "index"),
     path('user/', include('userauths.urls')),
-  
+    path('categories/', category_list_view, name='category-list'),
+    path('products/', product_list_view, name='product-list'),
+    path('vendors/', vendor_list_view, name='vendor-list'),
+     path('vendor/<vid>/', vendor_detail_view, name='vendor-detail'),
+
+
+
 ]
