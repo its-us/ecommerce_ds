324b5043c09adcb5da07a10132f0d6590ca0b6a7
Sihame Alami <sihamealami21@gmail.com>
2024-01-27 23:16:37 +0100

    filtre
    filter

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -93,33 +93,71 @@ $("#commentForm").submit(function(e){
 
 $(document).ready(function(){
     $(".loader").hide()
-    $(".filter-checkbox").on("click", function(){
+    $(".filter-checkbox, #price-filter-btn").on("click", function(){
         let filter_object = {}
-            $(".filter-checkbox").each(function(index) {
-                let filter_value = $(this).val()
-                let filter_key = $(this).data("filter")
-                
-                //console.log(filter_value, filter_key);
-                filter_object[filter_key] =
-                    Array.from(document.querySelectorAll('input[data-filter='+ filter_key +']:checked')).map(function(element){
-                        return element.value
-                })
-            })
-            console.log(filter_object);
-            $.ajax({
-                url : '/filter-products',
-                data : filter_object,
-                dataType:'json',
-                beforeSend: function(){
-                    console.log("Trying to filter products ...");
 
-                },
-                success: function(response){
-                    console.log(response);
-                    console.log("Data filtred successfully...");
-                    $("#filtered-products").html(response.data)
-                }
+        let min_price =$("#max_price").attr("min")
+        let max_price =$("#max_price").val()
+        filter_object.min_price = min_price;
+        filter_object.max_price = max_price;
+
+        $(".filter-checkbox").each(function(index) {
+            let filter_value = $(this).val()
+            let filter_key = $(this).data("filter")
+                
+            //console.log(filter_value, filter_key);
+            filter_object[filter_key] =
+                Array.from(document.querySelectorAll('input[data-filter='+ filter_key +']:checked')).map(function(element){
+                    return element.value
             })
+        })
+        console.log(filter_object);
+        $.ajax({
+            url : '/filter-products',
+            data : filter_object,
+            dataType:'json',
+            beforeSend: function(){
+                console.log("Trying to filter products ...");
+
+            },
+            success: function(response){
+                console.log(response);
+                console.log("Data filtred successfully...");
+                $("#filtered-products").html(response.data)
+            }
+        })
+    })
+
+    $("#max_price").on("blur", function(){
+
+        let min_price = $(this).attr("min")
+        let max_price = $(this).attr("max")
+        let current_price = $(this).val()
+
+        //console.log("Crurrent Price is:", current_price);
+        //console.log("Max Price is:", min_price);
+        //console.log("Min Price is:", max_price);
+
+        if(current_price < parseInt(min_price) || current_price > parseInt(max_price)){
+            //console.log("Price Error Occured");
+
+            min_price = Math.round(min_price * 100) / 100
+            max_price = Math.round(max_price * 100) / 100
+
+            //console.log("Max Price is:", min_Price);
+            //console.log("Min Price is:", max_Price);
+
+            alert("Price must be between $"+ min_price+' and $'+ max_price)
+            $(this).val(min_price)
+            $('#range').val(min_price)
+            $(this).focus()
+
+            return false
+            
+
+        }
+
+        
     })
 
 
