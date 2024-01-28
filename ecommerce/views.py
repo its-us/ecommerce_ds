dfe56b2bcf3dfec3eea9e549de668ca86696c9b9
ilyas sacha <ilyassacha2@gmail.com>
2024-01-28 12:08:40 +0100

    checkout

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -325,9 +325,15 @@ def checkout_view(request):
 
 
 def payment_completed_view(request):
-    return render(request,'ecommerce/payment-completed.html')
+    cart_total_amount = 0
+    if 'cart_data_obj' in request.session:
+        for p_id, item in request.session['cart_data_obj'].items():
+           cart_total_amount += int(item['qty']) * float(item['price'])
+        return render(request, 'ecommerce/payment-completed.html', {'cart_data': request.session['cart_data_obj'], 'totalcartitems': len(request.session['cart_data_obj']), 'cart_total_amount': cart_total_amount})
 
 
 def payment_failed_view(request):
     return render(request,'ecommerce/payment-failed.html')
     
+
+
